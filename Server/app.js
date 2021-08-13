c65d17b2cb15aae43ff6ef24650b387026c41e47
Yadav Dhakal <yadavdhkl@gmail.com>
2021-08-12 21:47:51 -0500

    Model, router for product

--- Server/app.js
+++ Server/app.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 require('dotenv/config');
 
-const api = process.env.API_URL;
 
-app.get(api + '/', (req, res)=>{
-    res.send('Hello API');
+//Middelware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+
+//Routes
+
+
+const productRouter = require('./routers/products');
+
+const api = process.env.API_URL;
+app.use(`${api}/products`, productRouter);
+
+
+//Database connection
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    dbName: 'oshop'
+})
+.then(()=>{
+    console.log('Database connection established');
+})
+.catch((err)=>{
+    console.log(err);
 });
 
+//Server
 app.listen(3000,  ()=>{
     console.log(api);
     console.log('Server is running on port http://localhost:3000');
